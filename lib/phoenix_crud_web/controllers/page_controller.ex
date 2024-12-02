548c0034481e0b7808192d9017edafeaea5a63b7
Wojtazz <marcin.witas72@gmail.com>
2024-12-02 18:19:00 +0100

    add custom html page

--- lib/phoenix_crud_web/controllers/page_controller.ex
+++ lib/phoenix_crud_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule PhoenixCrudWeb.PageController do
   use PhoenixCrudWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :test, layout: false)
   end
 end
